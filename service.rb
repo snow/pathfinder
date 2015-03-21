3501c08110836fc8a1c2f1eaf69d5c77ebfa4c26
Snow Hellsing <snow.hellsing@gmail.com>
2015-03-22 02:20:55 +0800

    CHG: drop caching, use dnsmasq instead

--- service.rb
+++ service.rb
@@ -10,65 +10,20 @@ HOME_STREAM = RubyDNS::Resolver.new(CONF[:home_resolvers].map{ |e|
   [:udp, ip, 53]
 })
 PROXY_STREAM = RubyDNS::Resolver.new [[:udp, '127.0.0.1', 40]]
+INTERFACES = [[:udp, '127.0.0.1', 5300], [:tcp, '127.0.0.1', 5300]]
 
-class Server < RubyDNS::RuleBasedServer
-  def initialize *ars, &block
-    super
+RubyDNS::run_server listen: INTERFACES do
+  #logger.level = ::Logger::INFO
 
-    @cache = {
-      'Resolv::DNS::Resource::IN::A' => {},
-      'Resolv::DNS::Resource::IN::AAAA' => {},
-    }
-  end
-
-  def respond transaction, stream
-    addr, expire_at = get_cached transaction
-
-    #logger.warn "#{expire_at}/#{Time.now.to_i}"
-    if expire_at && expire_at > Time.now.to_i
-      ttl = expire_at - Time.now.to_i
-      #logger.warn "respond from cache: #{transaction.question.to_s}, #{get_cached transaction}, #{ttl}"
-      transaction.respond! addr, ttl: ttl
-
-    else
-      transaction.passthrough!(stream) { |response| cache response }
-    end
-  end
-
-  def cache response
-    response.answer.\
-      select{ |name, ttl, res|
-        (res.kind_of?(Resolv::DNS::Resource::IN::A) ||
-            res.kind_of?(Resolv::DNS::Resource::IN::AAAA)) &&
-          ttl > 0
-      }.\
-      each { |name, ttl, res|
-        # name may be diffrent from response.question when target has CNAME
-        key = response.question[0][0].to_s.sub /\.$/, ''
-        @cache[res.class.name][key] = [res.address.to_s, Time.now.to_i + ttl]
-        #logger.warn CACHE
-        return
-      }
-  end
-
-  def get_cached transaction
-    res_class = transaction.resource_class.name
-    @cache.key?(res_class) and
-      @cache[res_class][transaction.question.to_s.sub /\.$/, '']
-  end
-end
-
-# Start the RubyDNS server
-RubyDNS::run_server listen: [[:udp, '127.0.0.1', 53]], server_class: Server do
   CONF[:direct_domains].each do |e|
     domain = e.split('#').first.strip
 
     match(/^(.+\.)?#{domain.gsub '.', '\.'}$/) { |transaction|
-      respond transaction, HOME_STREAM
+      transaction.passthrough! HOME_STREAM
     }
   end
 
   # ship anything else abroad
-  otherwise { |transaction| respond transaction, PROXY_STREAM }
+  otherwise { |transaction| transaction.passthrough! PROXY_STREAM }
 end
 
