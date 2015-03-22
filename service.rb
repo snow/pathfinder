723bab467bd8d87af642ce341b0f3c41fc071d2b
Snow Hellsing <snow.hellsing@gmail.com>
2015-03-22 18:21:08 +0800

    CHG: use home domain list from https://github.com/Leask/Flora_Pac

--- service.rb
+++ service.rb
@@ -5,25 +5,50 @@ require 'yaml'
 
 # Use upstream DNS for name resolution.
 CONF = YAML.load_file File.expand_path '../conf.yml', __FILE__
+
+HOME_DOMAINS_REGX = /#{File.readlines(File.expand_path '../home_domains.txt', __FILE__).map{ |e| "^(.+\.)?#{e.strip.gsub '.', '\.'}$"  }.join '|'}/
+
 HOME_STREAM = RubyDNS::Resolver.new(CONF[:home_resolvers].map{ |e|
   ip = e.split('#').first.strip
   [:udp, ip, 53]
 })
 PROXY_STREAM = RubyDNS::Resolver.new [[:udp, '127.0.0.1', 40]]
+
 INTERFACES = [[:udp, '127.0.0.1', 5300], [:tcp, '127.0.0.1', 5300]]
 
-RubyDNS::run_server listen: INTERFACES do
-  #logger.level = ::Logger::INFO
+class CustomDnsServer < RubyDNS::RuleBasedServer
+  DEBUG = true
 
-  CONF[:direct_domains].each do |e|
-    domain = e.split('#').first.strip
+  def deliver_to_stream transaction, stream
+    if DEBUG
+      q = transaction.question.to_s.sub /\.$/, ''
+      stream_name = stream.instance_variable_get("@servers").slice(0, 2).to_s
 
-    match(/^(.+\.)?#{domain.gsub '.', '\.'}$/) { |transaction|
-      transaction.passthrough! HOME_STREAM
-    }
+      transaction.passthrough!(stream) { |response|
+        logger.info "got #{q} from #{stream_name}"
+        response.answer.\
+          select{ |name, ttl, res|
+            (res.kind_of?(Resolv::DNS::Resource::IN::A) ||
+                res.kind_of?(Resolv::DNS::Resource::IN::AAAA)) &&
+              ttl > 0
+          }.\
+          each { |name, ttl, res|
+            logger.info "#{q} -> #{res.address.to_s}"
+          }
+      }
+    else
+      transaction.passthrough! stream
+    end
   end
+end
 
-  # ship anything else abroad
-  otherwise { |transaction| transaction.passthrough! PROXY_STREAM }
+RubyDNS::run_server server_class: CustomDnsServer, listen: INTERFACES do
+  logger.level = ::Logger::INFO
+
+  #logger.info HOME_DOMAINS_REGX
+
+  match(HOME_DOMAINS_REGX) { |transaction| deliver_to_stream transaction, HOME_STREAM }
+
+  otherwise { |transaction| deliver_to_stream transaction, PROXY_STREAM }
 end
 
