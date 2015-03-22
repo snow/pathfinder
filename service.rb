142faf632255f8663ae4408a8d48eb1db098a3c3
Snow Hellsing <snow.hellsing@gmail.com>
2015-03-22 18:47:30 +0800

    MF

--- service.rb
+++ service.rb
@@ -3,10 +3,10 @@
 require 'rubydns'
 require 'yaml'
 
+DEBUG = ARGV.select{ |arg| %w(debug -d).include? arg.downcase  }.any?
+
 # Use upstream DNS for name resolution.
 CONF = YAML.load_file File.expand_path '../conf.yml', __FILE__
-
-HOME_DOMAINS_REGX = /#{File.readlines(File.expand_path '../home_domains.txt', __FILE__).map{ |e| "^(.+\.)?#{e.strip.gsub '.', '\.'}$"  }.join '|'}/
 
 HOME_STREAM = RubyDNS::Resolver.new(CONF[:home_resolvers].map{ |e|
   ip = e.split('#').first.strip
@@ -17,8 +17,6 @@ PROXY_STREAM = RubyDNS::Resolver.new [[:udp, '127.0.0.1', 40]]
 INTERFACES = [[:udp, '127.0.0.1', 5300], [:tcp, '127.0.0.1', 5300]]
 
 class CustomDnsServer < RubyDNS::RuleBasedServer
-  DEBUG = true
-
   def deliver_to_stream transaction, stream
     if DEBUG
       q = transaction.question.to_s.sub /\.$/, ''
@@ -40,14 +38,22 @@ class CustomDnsServer < RubyDNS::RuleBasedServer
       transaction.passthrough! stream
     end
   end
+
+  def home_domains_regx
+    @home_domains_regx ||= /#{File.readlines(
+                                  File.expand_path '../home_domains.txt', __FILE__
+                                ).\
+                                select{ |e| !e.strip.start_with? '#' }.\
+                                map{ |e| e.split('#').first.strip }.\
+                                map{ |e| "^(.+\.)?#{e.gsub '.', '\.'}$" }.\
+                                join '|'}/
+  end
 end
 
 RubyDNS::run_server server_class: CustomDnsServer, listen: INTERFACES do
   logger.level = ::Logger::INFO
 
-  #logger.info HOME_DOMAINS_REGX
-
-  match(HOME_DOMAINS_REGX) { |transaction| deliver_to_stream transaction, HOME_STREAM }
+  match(home_domains_regx) { |transaction| deliver_to_stream transaction, HOME_STREAM }
 
   otherwise { |transaction| deliver_to_stream transaction, PROXY_STREAM }
 end
