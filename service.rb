ce6028504023833344b8bcca95ee2f338c1d1f39
Snow Hellsing <snow.hellsing@gmail.com>
2015-03-22 20:50:47 +0800

    FIX: avoid "Celluloid::DeadActorError: attempted to call a dead actor"

--- service.rb
+++ service.rb
@@ -8,22 +8,17 @@ DEBUG = ARGV.select{ |arg| %w(debug -d).include? arg.downcase  }.any?
 # Use upstream DNS for name resolution.
 CONF = YAML.load_file File.expand_path '../conf.yml', __FILE__
 
-HOME_STREAM = RubyDNS::Resolver.new(CONF[:home_resolvers].map{ |e|
-  ip = e.split('#').first.strip
-  [:udp, ip, 53]
-})
-PROXY_STREAM = RubyDNS::Resolver.new [[:udp, '127.0.0.1', 40]]
-
 INTERFACES = [[:udp, '127.0.0.1', 5300], [:tcp, '127.0.0.1', 5300]]
 
 class CustomDnsServer < RubyDNS::RuleBasedServer
-  def deliver_to_stream transaction, stream
+  def deliver_to_stream transaction, stream_type
+    stream = get_stream stream_type
+
     if DEBUG
       q = transaction.question.to_s.sub /\.$/, ''
-      stream_name = stream.instance_variable_get("@servers").slice(0, 2).to_s
 
       transaction.passthrough!(stream) { |response|
-        logger.info "got #{q} from #{stream_name}"
+        logger.info "got #{q} from #{stream_type}"
         response.answer.\
           select{ |name, ttl, res|
             (res.kind_of?(Resolv::DNS::Resource::IN::A) ||
@@ -39,6 +34,7 @@ class CustomDnsServer < RubyDNS::RuleBasedServer
     end
   end
 
+  private
   def home_domains_regx
     @home_domains_regx ||= /#{File.readlines(
                                   File.expand_path '../home_domains.txt', __FILE__
@@ -48,13 +44,34 @@ class CustomDnsServer < RubyDNS::RuleBasedServer
                                 map{ |e| "^(.+\.)?#{e.gsub '.', '\.'}$" }.\
                                 join '|'}/
   end
+
+  def get_stream type
+    case type
+    when :home
+      # when computer goes sleep, Resolver actor dies for Errno::ENETDOWN
+      # so recreate when it awakes
+      # otherwise will see Celluloid::DeadActorError
+      @home_stream = nil if @home_stream && !@home_stream.alive?
+
+      @home_stream ||= RubyDNS::Resolver.new(CONF[:home_resolvers].map{ |e|
+        ip = e.split('#').first.strip
+        [:udp, ip, 53]
+      })
+    when :proxy
+      @proxy_stream = nil if @proxy_stream && !@proxy_stream.alive?
+
+      @proxy_stream ||= RubyDNS::Resolver.new [[:udp, '127.0.0.1', 40]]
+    else
+      raise 'deliver_to_stream only supports :home and :proxy'
+    end
+  end
 end
 
 RubyDNS::run_server server_class: CustomDnsServer, listen: INTERFACES do
   logger.level = ::Logger::INFO
 
-  match(home_domains_regx) { |transaction| deliver_to_stream transaction, HOME_STREAM }
+  match(home_domains_regx) { |transaction| deliver_to_stream transaction, :home }
 
-  otherwise { |transaction| deliver_to_stream transaction, PROXY_STREAM }
+  otherwise { |transaction| deliver_to_stream transaction, :proxy }
 end
 
