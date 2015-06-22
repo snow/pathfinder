96e114e3d0f9baa9daca2c4ee0c669ff4f19666f
Snow Helsing <snow.helsing@gmail.com>
2015-06-22 16:42:50 +0800

    CHG: clean up

--- generate_pac.rb
+++ generate_pac.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'ipaddr'
 require 'slop'
+
 OPTS = Slop.parse do |o|
   o.banner = 'Usage: ./generate_pac.rb [-f] PROXY'
   o.separator ''
@@ -15,68 +17,38 @@ if OPTS.arguments.empty?
 end
 
 TEMPLATE = File.open(File.expand_path '../pac_template.js', __FILE__).read
-CN_IP_FILE = File.expand_path '../cn_ips.txt', __FILE__
+CN_NETS_FILE = File.expand_path '../cn_nets.txt', __FILE__
 
-IP_RECORDS_URL = 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
-CN_REGX = /apnic\|cn\|ipv4\|[0-9\.]+\|[0-9]+\|[0-9]+\|a.*/i
-
-def fetch_cn_ips
-  require 'open-uri'
-  require 'ipaddr'
-
+def parse_cn_nets
   results = []
   prev_net = ''
-  #CN_RECORDS = File.open(File.expand_path '../delegated-apnic-latest', __FILE__).read.scan CN_REGX
-  puts "fetching #{IP_RECORDS_URL}"
-  open(IP_RECORDS_URL){ |f| f.read }.tap{
-    puts "parsing..."
-  }.scan(CN_REGX).each do |record|
-    cols = record.split '|' # => apnic|CN|ipv4|1.50.0.0|65536|20100902|allocated
-    starting_ip = cols[3]
-    ips_count = cols[4].to_i
+  File.readlines(CN_NETS_FILE).each do |ln|
+    ip = IPAddr.new(ln.strip.sub /\.\d+\.\d+\//, '.0.0/')
 
-    # TODO: maybe there are better approch in ruby?
-    imask = (0xffffffff ^ (ips_count - 1)).to_s 16
-    mask = "#{imask[0..1].hex}.#{imask[2..3].hex}.0.0"
-
-    # mask in *nix format
-    # it's not used for now
-    mask2 = 32 - Math.log(ips_count, 2).to_i
-
+    starting_ip = ip.to_s
     unless starting_ip.end_with? '.0.0'
       starting_ip.sub! /\.\d+\.\d+$/, '.0.0'
     end
 
     unless starting_ip.eql? prev_net
-      results << [IPAddr.new(starting_ip).to_i, IPAddr.new(mask).to_i, mask2]
+      results << [ip.to_i, ip.instance_variable_get('@mask_addr')]
       prev_net = starting_ip
     end
   end
 
-  results << [IPAddr.new('127.0.0.1').to_i, IPAddr.new('255.0.0.0').to_i, 0]
-  results << [IPAddr.new('10.0.0.0').to_i, IPAddr.new('255.0.0.0').to_i, 0]
-  results << [IPAddr.new('172.16.0.0').to_i, IPAddr.new('255.240.0.0').to_i, 0]
-  results << [IPAddr.new('192.168.0.0').to_i, IPAddr.new('255.255.0.0').to_i, 0]
+  %w(127.0.0.1/8 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16 192.168.10.0/8).each { |e|
+    ip = IPAddr.new e
+    results << [ip.to_i, ip.instance_variable_get('@mask_addr')]
+  }
 
-  results.sort!{ |x, y| x[0] <=> y[0] }
+  results.sort{ |x, y| x[0] <=> y[0] }.map{ |e| "[#{e[0]}, #{e[1]}]" }
 
-  File.open(CN_IP_FILE, 'w+') do |f|
-    results.each { |e|
-      f.puts "[#{e[0]}, #{e[1]}]"
-    }
-  end
-
-  puts "CN ips saved to #{CN_IP_FILE}"
+  #puts "CN ips saved to #{CN_IP_FILE}"
 end
 
-if OPTS.force_fetch? || !File.exists?(CN_IP_FILE)
-  fetch_cn_ips
-end
-CN_IPS = File.open(CN_IP_FILE).readlines.\
-  map{ |ln| ln.strip }.\
-  join ",\n#{' ' * 8}"
+CN_IPS = parse_cn_nets.join ",\n#{' ' * 8}"
 
-HOME_DOMAINS = File.open(File.expand_path '../home_domains.txt', __FILE__).readlines.\
+HOME_DOMAINS = File.readlines(File.expand_path '../home_domains.txt', __FILE__).\
   map{ |ln| "'#{ln.strip}' : 1" }.\
   join ",\n#{' ' * 8}"
 
